83587c318b57b65d8a063c6b680f18c5e9fe3634
deepatesting <kspdeepa82@gmail.com>
2022-05-10 10:15:13 +0100

    Initial commit with Delete Mapping Task.

---
+++ src/main/java/com/techreturners/bookmanager/service/BookManagerService.java
@@ -0,0 +1,19 @@
+package com.techreturners.bookmanager.service;
+
+import com.techreturners.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookManagerService {
+
+    List<Book> getAllBooks();
+    Book insertBook(Book book);
+    Book getBookById(Long id);
+
+    //User Story 4 - Update Book By Id Solution
+    void updateBookById(Long id, Book book);
+
+    //Optional Task - User Story - Delete Book By Id Solution
+    void deleteBookById(Long id);
+
+}
